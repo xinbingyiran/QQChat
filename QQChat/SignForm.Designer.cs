cb9b8c5867ec59b737d6f1fc6d62353786f0f63b
hlkitty <hlkitty@yeah.net>
2016-07-05 16:11:02 +0800

    A: sign to number

--- QQChat/SignForm.Designer.cs
+++ QQChat/SignForm.Designer.cs
@@ -39,6 +39,8 @@
             this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -98,16 +100,16 @@
             // 
             // columnHeader6
             // 
-            this.columnHeader6.Text = "今天签到";
+            this.columnHeader6.Text = "签到名次";
             this.columnHeader6.Width = 63;
             // 
             // columnHeader7
             // 
-            this.columnHeader7.Text = "签到名次";
+            this.columnHeader7.Text = "签到时间";
             // 
             // columnHeader8
             // 
-            this.columnHeader8.Text = "签到时间";
+            this.columnHeader8.Text = "更新时间";
             this.columnHeader8.Width = 68;
             // 
             // button2
@@ -120,17 +122,38 @@
             this.button2.Text = "签到";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(12, 336);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Location = new System.Drawing.Point(118, 334);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "指定签到";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // SignForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(605, 365);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "SignForm";
             this.Text = "SignForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -146,5 +169,7 @@
         private System.Windows.Forms.ColumnHeader columnHeader7;
         private System.Windows.Forms.ColumnHeader columnHeader8;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
--- QQChat/SignForm.cs
+++ QQChat/SignForm.cs
@@ -16,12 +16,28 @@ namespace QQChat
     public partial class SignForm : Form
     {
         private QQ_Base _qq;
-
+        private CancellationTokenSource _cts;
+        private CancellationTokenSource _cts2;
         public SignForm()
         {
             InitializeComponent();
             this.button1.Click += Button1_Click;
             this.button2.Click += Button2_Click;
+            this.button3.Click += Button3_Click;
+            this.listView1.HideSelection = false;
+            this.FormClosed += SignForm_FormClosed;
+        }
+
+        private void SignForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if(this._cts != null && !this._cts.IsCancellationRequested)
+            {
+                this._cts.Cancel();
+            }
+            if (this._cts2 != null && !this._cts2.IsCancellationRequested)
+            {
+                this._cts2.Cancel();
+            }
         }
 
         private void Button1_Click(object sender, EventArgs e)
@@ -34,75 +50,276 @@ namespace QQChat
             this.QunSign(true);
         }
 
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            this.QunAdvSign();
+        }
+
         private void QunSign(bool doSign)
         {
-            if(listView1.SelectedItems.Count == 0)
+            if (this._cts != null && !this._cts.IsCancellationRequested)
+            {
+                this._cts.Cancel();
+                return;
+            }
+            if (listView1.SelectedItems.Count == 0)
             {
                 return;
             }
             var items = new List<ListViewItem>();
-            foreach(ListViewItem item in this.listView1.SelectedItems)
+            foreach (ListViewItem item in this.listView1.SelectedItems)
             {
                 if (item == null)
                 {
                     continue;
                 }
                 var qg = item.Tag as QunGroup;
-                if(qg == null)
+                if (qg == null)
                 {
                     continue;
                 }
                 items.Add(item);
             }
-            Task.Factory.StartNew(()=>QunSignTask(items, doSign));
+            this._cts = new CancellationTokenSource();
+            Task.Factory.StartNew(() => QunSignTask(items, doSign));
+        }
+        private void QunAdvSign()
+        {
+            if (this._cts2 != null && !this._cts2.IsCancellationRequested)
+            {
+                this._cts2.Cancel();
+                return;
+            }
+            if (listView1.SelectedItems.Count == 0)
+            {
+                return;
+            }
+            var items = new List<ListViewItem>();
+            foreach (ListViewItem item in this.listView1.SelectedItems)
+            {
+                if (item == null)
+                {
+                    continue;
+                }
+                var qg = item.Tag as QunGroup;
+                if (qg == null)
+                {
+                    continue;
+                }
+                items.Add(item);
+            }
+            int number = 0;
+            if (!int.TryParse(textBox1.Text, out number))
+            {
+                MessageBox.Show("请输入一个数字");
+                return;
+            }
+            if (number <= 0 || number > 2000)
+            {
+                MessageBox.Show("请输入有效数字");
+                return;
+            }
+            this._cts2 = new CancellationTokenSource();
+            Task.Factory.StartNew(() => QunSignAdvTask(items, number));
         }
 
         private void QunSignTask(List<ListViewItem> items, bool doSign)
         {
-            foreach(var item in items)
+            var cts = this._cts;
+            if (cts == null || cts.IsCancellationRequested)
             {
+                return;
+            }
+            foreach (var item in items)
+            {
+                if (cts == null || cts.IsCancellationRequested)
+                {
+                    return;
+                }
                 var qg = item.Tag as QunGroup;
-                var dc = _qq.QunSign(qg.groupid.ToString(),doSign);
-                if(dc != null && dc.ContainsKey("ec") && dc["ec"].ToString() == "0")
+                var dc = _qq.QunSign(qg.groupid.ToString(), doSign);
+                if (dc != null && dc.ContainsKey("ec") && dc["ec"].ToString() == "0")
                 {
                     var is_sign = dc["is_sign"].ToString();
 
-                    this.UpdateListViewItem(item,new string[] {
+                    this.UpdateListViewItem(item, new string[] {
                         item.SubItems[0].Text,
                         item.SubItems[1].Text,
                         dc["today_count"].ToString(),
                         dc["total_count"].ToString(),
                         dc["conti_count"].ToString(),
-                        is_sign,
                         is_sign == "0" ? "-" : dc["rank"].ToString(),
                         is_sign == "0" ? "-" : QQHelper.ToTime(long.Parse(dc["sign_time"].ToString())).ToLongTimeString(),
+                        DateTime.Now.ToLongTimeString()
                     });
                 }
+                else
+                {
+                    break;
+                }
+            }
+            if (cts != null && !cts.IsCancellationRequested)
+            {
+                cts.Cancel();
+            }
+        }
+        private void QunSignAdvTask(List<ListViewItem> items, int number)
+        {
+            var cts = this._cts2;
+            if (cts == null || cts.IsCancellationRequested)
+            {
+                return;
+            }
+            if (items.Count == 0)
+            {
+                return;
+            }
+            var item = items[0];
+            var lastqd = -1;
+            while (true)
+            {
+                if (cts == null || cts.IsCancellationRequested)
+                {
+                    return;
+                }
+                var qg = item.Tag as QunGroup;
+                var dc = _qq.QunSign(qg.groupid.ToString(), false);
+                var qd = 0;
+                if (dc != null && dc.ContainsKey("ec") && dc["ec"].ToString() == "0")
+                {
+                    var is_sign = dc["is_sign"].ToString();
+                    if(is_sign != "0")
+                    {
+                        this.UpdateListViewItem(item, new string[] {
+                        item.SubItems[0].Text,
+                        item.SubItems[1].Text,
+                        dc["today_count"].ToString(),
+                        dc["total_count"].ToString(),
+                        dc["conti_count"].ToString(),
+                        is_sign == "0" ? "-" : dc["rank"].ToString(),
+                        is_sign == "0" ? "-" : QQHelper.ToTime(long.Parse(dc["sign_time"].ToString())).ToLongTimeString(),
+                        DateTime.Now.ToLongTimeString()
+                        });
+                        break;
+                    }
+                    if (!int.TryParse(dc["today_count"].ToString(), out qd))
+                    {
+                        break;
+                    }
+                    if (qd < number - 1)
+                    {
+                        this.UpdateListViewItem(item, new string[] {
+                        item.SubItems[0].Text,
+                        item.SubItems[1].Text,
+                        dc["today_count"].ToString(),
+                        dc["total_count"].ToString(),
+                        dc["conti_count"].ToString(),
+                        is_sign == "0" ? "-" : dc["rank"].ToString(),
+                        is_sign == "0" ? "-" : QQHelper.ToTime(long.Parse(dc["sign_time"].ToString())).ToLongTimeString(),
+                        DateTime.Now.ToLongTimeString()
+                        });
+                        if (cts == null || cts.IsCancellationRequested)
+                        {
+                            return;
+                        }
+                        var waittime = 10;
+                        if(lastqd == qd)
+                        {
+                            waittime = (number - 1 - qd) * 500;
+                            if (waittime > 3000)
+                            {
+                                waittime = 3000;
+                            }
+                            if (waittime < 0)
+                            {
+                                waittime = 10;
+                            }
+                            cts.Token.WaitHandle.WaitOne(waittime);
+                        }
+                        else
+                        {
+                            lastqd = qd;
+                        }
+                        continue;
+                    }
+                    else if (qd > number - 1)
+                    {
+                        this.UpdateListViewItem(item, new string[] {
+                        item.SubItems[0].Text,
+                        item.SubItems[1].Text,
+                        dc["today_count"].ToString(),
+                        dc["total_count"].ToString(),
+                        dc["conti_count"].ToString(),
+                        is_sign == "0" ? "-" : dc["rank"].ToString(),
+                        is_sign == "0" ? "-" : QQHelper.ToTime(long.Parse(dc["sign_time"].ToString())).ToLongTimeString(),
+                        DateTime.Now.ToLongTimeString()
+                        });
+                        break;
+                    }
+                    else
+                    {
+                        dc = _qq.QunSign(qg.groupid.ToString(), true);
+                        if (dc != null && dc.ContainsKey("ec") && dc["ec"].ToString() == "0")
+                        {
+                            is_sign = dc["is_sign"].ToString();
+                            if (!int.TryParse(dc["today_count"].ToString(), out qd))
+                            {
+                                break;
+                            }
+                            if (qd < number - 1)
+                            {
+                                this.UpdateListViewItem(item, new string[] {
+                                item.SubItems[0].Text,
+                                item.SubItems[1].Text,
+                                dc["today_count"].ToString(),
+                                dc["total_count"].ToString(),
+                                dc["conti_count"].ToString(),
+                                is_sign == "0" ? "-" : dc["rank"].ToString(),
+                                is_sign == "0" ? "-" : QQHelper.ToTime(long.Parse(dc["sign_time"].ToString())).ToLongTimeString(),
+                                DateTime.Now.ToLongTimeString()
+                                });
+                            }
+                        }
+                        break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (cts != null && !cts.IsCancellationRequested)
+            {
+                cts.Cancel();
             }
         }
 
-        private void UpdateListViewItem(ListViewItem item,string[] texts)
+        private void UpdateListViewItem(ListViewItem item, string[] texts)
         {
-            if(InvokeRequired)
+            if (InvokeRequired)
             {
-                this.BeginInvoke((Action<ListViewItem,String[]>)UpdateListViewItem,new object[] { item, texts });
+                this.BeginInvoke((Action<ListViewItem, String[]>)UpdateListViewItem, new object[] { item, texts });
                 return;
             }
-            for(int i = 0;i < item.SubItems.Count && i < texts.Length;i ++)
+            if(this.IsDisposed)
+            {
+                return;
+            }
+            for (int i = 0; i < item.SubItems.Count && i < texts.Length; i++)
             {
                 item.SubItems[i].Text = texts[i];
             }
         }
 
-        internal void InitParas(QQ_Base qq,List<QunGroup> glist)
+        internal void InitParas(QQ_Base qq, List<QunGroup> glist)
         {
             this._qq = qq;
             this.listView1.Items.Clear();
             foreach (var g in glist)
             {
-                if(g != null)
+                if (g != null)
                 {
-                    this.listView1.Items.Add(new ListViewItem(new string[] { g.groupname, g.groupid.ToString(),"-", "-", "-", "-", "-", "-" }) { Tag = g });
+                    this.listView1.Items.Add(new ListViewItem(new string[] { g.groupname, g.groupid.ToString(), "-", "-", "-", "-", "-", "-" }) { Tag = g });
                 }
             }
         }
